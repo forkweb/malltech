f40c488cc378d3206185202c0f87e4fb824a4329
fork_web <leshadgrad@mail.ru>
2020-10-26 15:00:40 +0400

    start

---
+++ src/components/Planeta.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="hello">
+    start
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style lang="scss" type="text/scss">
+  // @import "~@/style/main.scss";
+</style>
